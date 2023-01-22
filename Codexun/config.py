f3d6b808352a4a2d3a904599b50e4e79845cb381
❥︎ ɪᴛs'ᴀʀɪʏᴀɴ <115484197+Its-ariyan@users.noreply.github.com>
2023-01-22 20:11:15 +0600

    Update config.py

--- Codexun/config.py
+++ Codexun/config.py
@@ -1,7 +1,3 @@
-## Disclaimer By Team Codexun
-
-## Don't try to edit this file otherwise your bot will be crash.
-
 from os import getenv
 
 from dotenv import load_dotenv
@@ -10,31 +6,31 @@ load_dotenv()
 
 que = {}
 admins = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
-BOT_USERNAME = getenv("BOT_USERNAME")
-ASSID = int(getenv("ASSID"))
-ASSNAME = getenv("ASSNAME")
-ASSUSERNAME = getenv("ASSUSERNAME")
-BOT_ID = int(getenv("BOT_ID"))
+SESSION_NAME = getenv("SESSION_NAME", "BQCTT3XwCpeu2KD9VYNp27GjsuPSUuOblwAJsP-dZSssfXDsaxtN9fhYSo5yxadiut9Xyamfs6sURdB5yIG7hWc9i1-QM3v6xE4QG5scVn4XC0WGtIR1Sg5AsRcSYI6B7yEwX9WHwPkBhxI89SXkDUzj_dYmPubejCNHzc9C9fOOV7cPgWaW7_eIvd7UFQSdr0sWbZp3IsTikoyDCM-_2iTP-OCHvQBpLMOq4e61nzAwxTZvqxrtT1JzzYqmtqAGtO26qFBwxbSpbulCiObNYAcHzlCVQChj9EjchY9Pr8NfZCLVPeK3ahFmZlfyI-8dg6Ymw3ztLN5unITIutelZ28iAAAAAUYNJBMA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5708609169:AAGr85PVpvo6Yk06LrRSskbDoDmVCKoxFzU")
+BOT_NAME = getenv("BOT_NAME", "NixaX")
+BOT_USERNAME = getenv("BOT_USERNAME", "TagallXBot")
+ASSID = int(getenv("ASSID", "5470233619"))
+ASSNAME = getenv("ASSNAME", "SankiX")
+ASSUSERNAME = getenv("ASSUSERNAME", "NixaXAssistant")
+BOT_ID = int(getenv("BOT_ID", "5708609169"))
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PavanMagar/CodexunMusicBot")
-USERS = getenv("2056407064")
+USERS = getenv("5708609169")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-OWNER_ID = int(getenv("OWNER_ID"))
-UPDATE = getenv("UPDATE")
-SUPPORT = getenv("SUPPORT")
-START_IMG = getenv("START_IMG")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "100"))
-CMD_MUSIC = list(getenv("CMD_MUSIC", "/ !").split())
+API_ID = int(getenv("API_ID", "23842900"))
+API_HASH = getenv("API_HASH", "d21e95895cf2a5b83b0167fdd3b6e541")
+OWNER_ID = int(getenv("OWNER_ID", "5761513990"))
+UPDATE = getenv("UPDATE", "NixaWorld")
+SUPPORT = getenv("SUPPORT", "SankiWorldMF")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/808b95a6e24cd6f322092.jpg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
+CMD_MUSIC = list(getenv("CMD_MUSIC", "/ . !").split())
 BG_IMG = getenv("BG_IMG", "https://telegra.ph/file/f2a2d31f60a9e0f3dbe94.png")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2056407064").split()))
-ASSISTANT_NAME = getenv("ASSNAME")
+ASSISTANT_NAME = getenv("ASSNAME", "SankiX")
 COMMAND_PREFIXES = ("/ ! .").split()
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/a085a3cea21f994264152.jpg")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
